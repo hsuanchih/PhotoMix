3eac4e37e6965fb6f71d2eb4b3a85fc9bbc6888c
Chuang HsuanChih <hcc@email.com>
2015-05-12 13:49:51 +0800

    Initial Commit

---
+++ PhotoMix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoMix
+//
+//  Created by Chuang HsuanChih on 5/12/15.
+//  Copyright (c) 2015 Hsuan-Chih Chuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
